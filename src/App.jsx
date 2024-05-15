4dff2f8e291b7f4ef60755acf3e1514c8598c70c
Tushar Pandao <pandaotushar1@gmail.com>
2024-05-15 19:28:03 +0530

    added the new library

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,73 @@
-// import { useState } from 'react'
+import React from "react";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Cart from './pages/Cart/Cart'
-import Home from './pages/Home/Home'
-import AllProducts from './components/AllProducts/AllProducts'
-import SignIn from './pages/SignIn/SignIn'
-import SignUp from './pages/SignUp/SignUp'
-import Mens from './components/Mens/Mens'
-import { useState } from 'react'
-import Womens from './components/Womens/Womens'
-
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
+import AllProducts from "./components/AllProducts/AllProducts";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+import Mens from "./components/Mens/Mens";
+import { useState } from "react";
+import Womens from "./components/Womens/Womens";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  
-  const[cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
  
   const AddToCart = (product) => {
     // Check if the product is already in the cart
-    const existingProductIndex = cart.findIndex(item => item.id === product.id);
+    const existingProductIndex = cart.findIndex(
+      (item) => item.id === product.id
+    );
     
     if (existingProductIndex !== -1) {
-        // If the product exists, update its quantity
-        const updatedCart = [...cart];
-        updatedCart[existingProductIndex].quantity++;
-        setCart(updatedCart);
+      // If the product exists, update its quantity
+      const updatedCart = [...cart];
+      updatedCart[existingProductIndex].quantity++;
+      setCart(updatedCart);
     } else {
-        // If the product is not in the cart, add it with quantity 1
-        setCart([...cart, {...product, quantity: 1}]);
+      // If the product is not in the cart, add it with quantity 1
+      setCart([...cart, { ...product, quantity: 1 }]);
     }
-}
-
+  };
 
   return (
     <>
       <div>
         <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/cart" element={<Cart cart={cart} setCart={setCart}/>}/>
-          <Route path="/allProducts" element={<AllProducts AddToCart={AddToCart} />}/>
-          <Route path="/signUp" element={<SignUp/>}/>
-          <Route path="/signIn" element={<SignIn/>}/>
-          <Route path="/mens" element={<Mens  AddToCart={AddToCart}/>}/>
-          <Route path="/Womens" element={<Womens AddToCart={AddToCart}/>}/>
-          
+          <NavBar cart={cart}  />
+          <div className="flex-grow pt-[80px]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/cart"
+                element={<Cart cart={cart} setCart={setCart} />}
+              />
+              <Route
+                path="/allProducts"
+                element={<AllProducts AddToCart={AddToCart}/>}
+              />
+              <Route path="/signUp" element={<SignUp />} />
+              <Route
+                path="/signIn"
+                element={<SignIn />}
+              />
+              <Route path="/mens" element={<Mens AddToCart={AddToCart} />} />
+              <Route
+                path="/Womens"
+                element={<Womens AddToCart={AddToCart} />}
+              />
+            </Routes>
+          </div>
 
-          
-        </Routes>
-        
+          <Footer />
         </BrowserRouter>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
