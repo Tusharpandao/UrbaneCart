d26817e6ba6396a8690c274df0c34fd74eae7abd
Tushar Pandao <pandaotushar1@gmail.com>
2024-07-06 20:53:31 +0530

    add sign out button and user name using conditional rendoring

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,30 +5,31 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCrossCircled } from "react-icons/rx";
 import { CgShoppingCart } from "react-icons/cg";
 import { auth } from "../../FirebaseConfig/FirebaseConfig";
+import { useAuthState } from 'react-firebase-hooks/auth'; // Import the useAuthState hook
+import toast from "react-hot-toast";
+import { signOut } from "firebase/auth";
+
 
 const NavBar = ({ cart }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const [user, loading, error] = useAuthState(auth);   // Get user from auth state using react-firebase-hooks
+
+
   const toggleChange = () => {
     isOpen === false ? setIsOpen(true) : setIsOpen(false);
   };
-  useEffect(() => {
-    const user = auth.currentUser;
 
-    if (user !== null) {
-      user.providerData.forEach((profile) => {
-        console.log("Sign-in provider: " + profile.providerId);
-        console.log("  Provider-specific UID: " + profile.uid);
-        console.log("  Name: " + profile.displayName);
-        console.log("  Email: " + profile.email);
-        console.log("  Photo URL: " + profile.photoURL);
-      });
-    }
-  }, []);
-  // }
-  // const toggleClose=()=>{
-  //   setIsOpen(false)
-  // }
+  const handleSignOut = async () => {
+    // auth.signOut();
+
+    await signOut(auth).then(() => {
+      toast.success("You are signed out successfully");
+    }).catch((error) => {
+      toast.error(error.message);
+    });
+  };
+ 
   return (
     <header className="fixed top-0 w-full z-20 bg-white border-b border-gray-200">
       <div className="container mx-auto flex justify-between p-5 items-center">
@@ -98,22 +99,31 @@ const NavBar = ({ cart }) => {
         )}
 
         <div className="flex justify-center items-center gap-3">
-          <Link to="/signIn">
-            <button
-              className=" bg-red-300 border-0 py-1 px-3 focus:outline-none hover:bg-red-500
-               rounded text-base  font-semibold"
-            >
-              Sign In
-            </button>
-          </Link>
-          {/* <Link to="/cart">
-            <button className="relative inline-block">
-              <span className="absolute top-0 right-0 transform translate-x-1/3 -translate-y-1/3 px-1 bg-red-500 rounded-[50%]  text-sm font-bold">
-              {cart?.length ?? ''} 
-              </span>
-              <CgShoppingCart size={35} />
-            </button>
-          </Link> */}
+          {/* Sign in button */}
+         
+
+{user ? ( // Conditionally render if user is logged in
+      <div>
+        <span className="mr-3 text-lg font-semibold">{user.displayName || user.email}</span>
+        {/* Sign Out button  */}
+        <button
+          className=" bg-red-300 border-0 py-1 px-3 focus:outline-none hover:bg-red-500
+            rounded text-base  font-semibold"
+            onClick={handleSignOut}
+        >
+          Sign Out
+        </button>
+      </div>
+    ) : ( 
+      <Link to="/signIn">
+        <button
+          className=" bg-red-300 border-0 py-1 px-3 focus:outline-none hover:bg-red-500
+            rounded text-base  font-semibold"git 
+        >
+          Sign In
+        </button>
+      </Link>
+    )}
 
           {/* Conditional Cart Icon and Count */}
           {(cart?.length ?? 0) > 0 && ( // Only show if cart exists and has items
